3a813f4ee29ea8fdf5b127088aeac3174376c1b2
Szymon <szymonphotoshop@gmail.com>
2020-11-26 12:19:22 +0100

    added second page content to test cart functionality

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,16 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ShopItem from "../components/shopItem"
+import { graphql } from "gatsby"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const ProductsPage = ({ data }) => {
+  const { edges } = data.allStripePrice
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Products</h1>
+      {edges.map(node => (
+        <ShopItem key={node.id} price={node} />
+      ))}
+    </Layout>
+  )
+}
 
-export default SecondPage
+export default ProductsPage
+
+export const ProductsPageQuery = graphql`
+  query ProductsPageQuery {
+    allStripePrice(filter: { active: { eq: true } }) {
+      edges {
+        node {
+          id
+          product {
+            images
+            name
+          }
+          currency
+          unit_amount
+          unit_amount_decimal
+        }
+      }
+    }
+  }
+`
