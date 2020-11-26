3ee63768abe3f8c3d5dc4d1476771130ecf95e42
Szymon <szymonphotoshop@gmail.com>
2020-11-26 13:05:43 +0100

    added netlify cms to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,12 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
       resolve: `gatsby-source-stripe`,
       options: {
         objects: ["Product", "Price"],
