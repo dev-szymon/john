4a0eb8a83b3a65099698a996f2808ed8523c7b3f
Szymon <szymonphotoshop@gmail.com>
2021-02-05 19:35:09 +0100

    removed transition that caused problems

--- src/components/variantsInput/variantsInput.js
+++ src/components/variantsInput/variantsInput.js
@@ -5,18 +5,20 @@ const VariantsInput = ({ variants, onChange, label }) => {
   return (
     <div className="variants-input">
       <span>{label}</span>
-      {variants.map((option, i) => (
-        <input
-          key={`${i} variant ${option.color}`}
-          className="color-option"
-          type="radio"
-          name={label}
-          value={option}
-          defaultChecked={i === 0}
-          onClick={() => onChange(option)}
-          style={{ backgroundColor: option.color }}
-        />
-      ))}
+      <div className="variants-input__wrapper">
+        {variants.map((option, i) => (
+          <input
+            key={`${i} variant ${option.color}`}
+            className="color-option"
+            type="radio"
+            name={label}
+            value={option}
+            defaultChecked={i === 0}
+            onClick={() => onChange(option)}
+            style={{ backgroundColor: option.color }}
+          />
+        ))}
+      </div>
     </div>
   )
 }
